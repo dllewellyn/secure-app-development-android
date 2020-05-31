85d4873c2ce1bff9d93044f263f002a7a75767a0
danielllewellyn <danieln.llewellyn@gmail.com>
2020-05-31 12:14:22 +0100

    Initial commit

---
+++ app/src/main/java/com/secure/app/dev/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.secure.app.dev
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.secure.app.dev.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+}
